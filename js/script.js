a4b57e4700f2a61e831e77a77454043d08e131b0
AlessiaOsman <alessia.osman@gmail.com>
2024-01-11 16:03:11 +0100

    first milestone

--- js/script.js
+++ js/script.js
@@ -5,56 +5,67 @@ const selectElement = document.getElementById('select')
 
 const gridElement = document.getElementById('grid')
 
-/* Dati di partenza*/
+/*Preparo una variabile che tenga il punteggio dell'utente */
+
+let score = 0
+
 
 
 formElement.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    
+
     let userChoice = selectElement.value
 
     let rows = 10;
     let cols = 10;
-    
+
 
     let className = 'c-100'
 
-    if(userChoice === '2'){
+    if (userChoice === '2') {
         rows = 9
         cols = 9
         className = 'c-81'
-    } else if (userChoice === '3'){
-        rows = 7    
+    } else if (userChoice === '3') {
+        rows = 7
         cols = 7
         className = 'c-49'
     }
 
-    let totalCells = rows*cols
+    let totalCells = rows * cols
 
     while (gridElement.firstChild) {
         gridElement.removeChild(gridElement.firstChild);
-}
-    
-    for(let i = 0; i<totalCells; i++){
+    }
+
+    for (let i = 1; i <= totalCells; i++) {
 
         const cell = createCell(className)
-    
-        cell.addEventListener('click', ()=>{
-            cell.classList.toggle('clicked')
-            console.log(i+1)
+
+        cell.addEventListener('click', () => {
+            cell.classList.add('clicked')
+            console.log(i)
+            /*Incremento il punteggio al click sulla cella*/
+            score++
+            console.log(score)
+
         })
-    
+
         gridElement.appendChild(cell)
-        cell.innerText = i+1
+        cell.innerText = i
+
+
     }
-    
-   
-   
+
+
+
     console.log(userChoice)
 
-    
 
-    
+
+
 })
 
 
